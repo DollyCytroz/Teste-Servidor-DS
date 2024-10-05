9d76d89c7207f13b32d96abd17e1ff06b7ea2ac8
DollyCytroz <kauemcarreiro11@gmail.com>
2024-10-04 21:57:25 -0300

    Servidor com listen

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import http from 'http';
+import http from 'http'
 
 const PORT = 3300;
 
@@ -7,14 +7,14 @@ const rotas = {
     '/teste': 'ROTA DE TESTE',
     '/teste2': 'AAAAAAAAA'
 }
-const server = http
-
-
-server.createServer((req,res) => {
+const server = http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'}),
     res.end(rotas[req.url])
 })
 
-server.listen(PORT, ()=> {
-    console.log('SERVIDOR ESTÁ ONLINE')
+
+console.log('até aqui');
+
+server.listen(PORT,()=>{
+    console.log('aqui foi tmb')
 })
